d5bf523a42e2535f11b6218110e9826cc491a7a1
pravesh <praveshshrestha@Praveshs-MacBook-Pro.local>
2021-10-02 13:09:19 +0545

    first screen completed

---
+++ app/src/main/java/android/portfolio/petshuddle/UI/MainActivity.java
@@ -0,0 +1,15 @@
+package android.portfolio.petshuddle.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.portfolio.petshuddle.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
